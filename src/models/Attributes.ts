7cb391f6073f9ad09a206b012fcdbca057546ef3
mrincone <mayrajose.rinconessalgado@nttdata.com>
2022-09-02 13:31:05 -0300

    added Model and Collection Classes

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -3,11 +3,18 @@ export class Attributes<T> {
 
   // limits the values that key can be. For this project:
   // name, age, id
-  get<K extends keyof T>(key: K): T[K] {
+  get = <K extends keyof T>(key: K): T[K] => {
     return this.data[key];
-  }
+  };
+
+  // We turn get into a "bound" function (arrow function) in order to
+  // correctly bound "this" to the instance of the function
 
   set(update: T): void {
     Object.assign(this.data, update);
+  }
+
+  getAll(): T {
+    return this.data;
   }
 }
